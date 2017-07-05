dabced089d91c99afbd90a9829b657870f73bd26
github username <github email address>
2017-07-05 19:08:49 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,14 @@ class PostsController < ApplicationController
 		@post = Post.find(params[:id])
 	end
 
+	def create
+		@post = Post.new
+		@post.title = params[:title]
+		@post.description = params[:description]
+		@post.save
+		redirect_to post_path(@post)
+	end
+
 	def new
 		@post = Post.new
 	end
